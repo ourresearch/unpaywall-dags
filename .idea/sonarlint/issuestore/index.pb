e66bd243a398bb91382e97dd5fd8e6dcd799659e
Casey M <caseym@gmail.com>
2023-09-04 13:52:36 -0500

    improve dag

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,25 @@
+
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+B
+.astro/config.yaml,c/3/c3fc1f49bc1d4112d061a5a457769c0f2746ddef
+T
+$.astro/test_dag_integrity_default.py,e/f/ef38bf8ce6e96eb22776ed779cf802a93f27274c
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+=
+.dockerignore,0/5/05a721ebe3ba0754116825ed1359ff1785f4bbac
+<
+packages.txt,c/9/c92d1cff5fdc90f19d2be5ebba8f452f2a1e317c
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+@
+requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
+E
+airflow_settings.yaml,1/d/1d3820747db070b48e4a906adf54dba7ea40e86b
+4
+.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+O
+dags/export_daily_changefile.py,a/3/a386db322d94c8f8c858abde9f07e1d2235b9f8d
--- dags/export_daily_changefile.py
+++ dags/export_daily_changefile.py
@@ -1,97 +1,103 @@
 import gzip
+import logging
 
 from airflow.decorators import task, dag
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
 import heroku3
-from pendulum import datetime
+import pendulum
 
-JSON_STAGING_TABLE = "daily_export_staging"
+JSON_STAGING_TABLE = "daily_export_staging_test"
 DAILY_EXPORT_HISTORY = "daily_export_dates"
 
 
-@dag(schedule_interval="@daily", start_date=datetime(2023, 8, 23), catchup=False)
+@dag(schedule_interval="@daily", start_date=pendulum.datetime(2023, 9, 4), catchup=False)
 def export_daily_changefile():
 
     @task()
     def extract_changes(execution_date):
-        start_date = execution_date - days_ago(2)
-        end_date = execution_date
+        execution_date_dt = pendulum.parse(execution_date)
+        start_date = execution_date_dt.subtract(days=2).strftime('%Y-%m-%d')
+        end_date = execution_date_dt.strftime('%Y-%m-%d')
+        logging.info(f"Extracting daily snapshot changes from {start_date} to {end_date}")
 
-        conn_id = "your_postgres_connection_id"  # Replace with your Airflow connection ID
+        conn_id = "UNPAYWALL_POSTGRES"
         pg_hook = PostgresHook(conn_id)
 
-        sql_commands = [
-            f"TRUNCATE {JSON_STAGING_TABLE};",
-            f"""
+        truncate_sql = f"TRUNCATE {JSON_STAGING_TABLE};"
+        logging.info(f"Truncating {JSON_STAGING_TABLE}")
+        pg_hook.run(truncate_sql)
+
+        insert_sql = f"""
             INSERT INTO {JSON_STAGING_TABLE} (
                 SELECT pub.id, pub.updated, pub.last_changed_date, pub.response_jsonb
                 FROM pub 
                 LEFT JOIN {DAILY_EXPORT_HISTORY} history USING (id)
-                WHERE pub.last_changed_date BETWEEN {start_date} AND {end_date}
+                WHERE pub.last_changed_date BETWEEN %s AND %s
                 AND pub.updated > '1043-01-01'::timestamp
                 AND (history.last_exported_update IS NULL OR history.last_exported_update < pub.last_changed_date)
             );
             """
-        ]
 
-        for sql in sql_commands:
-            pg_hook.run(sql)
+        logging.info(f"Inserting daily snapshot changes from {start_date} to {end_date}")
+        pg_hook.run(insert_sql, parameters=(start_date, end_date))
 
-    @task
-    def export_gzip_and_upload_to_s3(execution_date):
-        pg_hook = PostgresHook(postgres_conn_id='your_postgres_conn_id')
-        s3_hook = S3Hook(aws_conn_id='your_aws_conn_id')
+        logging.info("Finished extracting daily snapshot changes")
 
-        # Generate the unique filename using execution_date
-        filename = f"changed_dois_with_versions_{execution_date.strftime('%Y-%m-%dT%H%M%S')}.jsonl.gz"
-        temp_filepath = f"/tmp/{filename}"
+    # @task
+    # def export_gzip_and_upload_to_s3(execution_date):
+    #     pg_hook = PostgresHook(postgres_conn_id='your_postgres_conn_id')
+    #     s3_hook = S3Hook(aws_conn_id='your_aws_conn_id')
+    #
+    #     # Generate the unique filename using execution_date
+    #     filename = f"changed_dois_with_versions_{execution_date.strftime('%Y-%m-%dT%H%M%S')}.jsonl.gz"
+    #     temp_filepath = f"/tmp/{filename}"
+    #
+    #     # Gzip and write data to the temp file
+    #     with gzip.open(temp_filepath, 'wb') as gz:
+    #         conn = pg_hook.get_conn()
+    #         cursor = conn.cursor()
+    #         sql = f"COPY (SELECT response_jsonb FROM {JSON_STAGING_TABLE} WHERE response_jsonb IS NOT NULL) TO STDOUT"
+    #         cursor.copy_expert(sql, gz)
+    #         cursor.close()
+    #
+    #     # Upload gzipped data to S3
+    #     s3_hook.load_file(
+    #         filename=temp_filepath,
+    #         key=filename,
+    #         bucket_name='unpaywall-daily-data-feed',
+    #         replace=True
+    #     )
+    #
+    # @task()
+    # def update_last_exported_dates():
+    #     # Get Postgres Hook
+    #     pg_hook = PostgresHook(postgres_conn_id='your_postgres_conn_id')
+    #
+    #     # SQL to update last-exported dates and truncate the staging table
+    #     update_sql = f"""
+    #         INSERT INTO {DAILY_EXPORT_HISTORY} (id, last_exported_update) (
+    #             SELECT id, last_changed_date FROM {JSON_STAGING_TABLE}
+    #         )
+    #         ON CONFLICT (id) DO UPDATE SET last_exported_update = excluded.last_exported_update;
+    #
+    #         TRUNCATE {JSON_STAGING_TABLE};
+    #         """
+    #
+    #     # Run SQL
+    #     pg_hook.run(update_sql)
+    #
+    # @task()
+    # def update_changefile_dicts():
+    #     heroku_conn = heroku3.from_key('HEROKU_API_KEY')
+    #     app = heroku_conn.apps()['oadoi']
+    #     app.run_command('python cache_changefile_dicts.py', attach=False)
 
-        # Gzip and write data to the temp file
-        with gzip.open(temp_filepath, 'wb') as gz:
-            conn = pg_hook.get_conn()
-            cursor = conn.cursor()
-            sql = f"COPY (SELECT response_jsonb FROM {JSON_STAGING_TABLE} WHERE response_jsonb IS NOT NULL) TO STDOUT"
-            cursor.copy_expert(sql, gz)
-            cursor.close()
-
-        # Upload gzipped data to S3
-        s3_hook.load_file(
-            filename=temp_filepath,
-            key=filename,
-            bucket_name='unpaywall-daily-data-feed',
-            replace=True
-        )
-
-    @task()
-    def update_last_exported_dates():
-        # Get Postgres Hook
-        pg_hook = PostgresHook(postgres_conn_id='your_postgres_conn_id')
-
-        # SQL to update last-exported dates and truncate the staging table
-        update_sql = f"""
-            INSERT INTO {DAILY_EXPORT_HISTORY} (id, last_exported_update) (
-                SELECT id, last_changed_date FROM {JSON_STAGING_TABLE}
-            )
-            ON CONFLICT (id) DO UPDATE SET last_exported_update = excluded.last_exported_update;
-
-            TRUNCATE {JSON_STAGING_TABLE};
-            """
-
-        # Run SQL
-        pg_hook.run(update_sql)
-
-    @task()
-    def update_changefile_dicts():
-        heroku_conn = heroku3.from_key('HEROKU_API_KEY')
-        app = heroku_conn.apps()['oadoi']
-        app.run_command('python cache_changefile_dicts.py', attach=False)
-
-    extract_task = extract_changes()
-    export_task = export_gzip_and_upload_to_s3()
-    update_dates_task = update_last_exported_dates()
-    update_dicts = update_changefile_dicts()
+    extract_task = extract_changes(execution_date="{{ ds }}")
+    # export_task = export_gzip_and_upload_to_s3(execution_date="{{ ds }}")
+    # update_dates_task = update_last_exported_dates()
+    # update_dicts_task = update_changefile_dicts()
 
 
 export_daily_changefile_dag = export_daily_changefile()
